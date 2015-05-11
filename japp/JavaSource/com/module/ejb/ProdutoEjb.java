06c66a254325fedb3814afced1b2085d22e750ba
jfeliperc <jfeliperc@gmail.com>
2015-05-11 18:23:27 -0300

    GC - 11052015

--- japp/JavaSource/com/module/ejb/ProdutoEjb.java
+++ japp/JavaSource/com/module/ejb/ProdutoEjb.java
@@ -60,7 +60,10 @@ public class ProdutoEjb implements IProdutoEjb {
 	public List<TipoProduto> buscarTipoProduto(TipoProduto tipoProduto) {
 		TipoProdutoDao dao = new TipoProdutoDao();
 		List<TipoProduto> ret = new ArrayList<TipoProduto>();
-		if ((tipoProduto.getId() == null)&&(tipoProduto.getNome() == null)&&(tipoProduto.getDescricao() == null)){
+		tipoProduto.setId(tipoProduto.getId() != null && tipoProduto.getId().intValue() == 0 ? null : tipoProduto.getId());
+		if ((tipoProduto.getId() == null)
+				&&((tipoProduto.getNome() == null)||("".equals(tipoProduto.getNome())))
+				&&((tipoProduto.getDescricao() == null)||("".equals(tipoProduto.getDescricao())))){
 			ret = dao.getAll();
 		}else{
 			ret = dao.findByExample(tipoProduto);
@@ -72,7 +75,11 @@ public class ProdutoEjb implements IProdutoEjb {
 	public List<GrupoProduto> buscarGrupoProduto(GrupoProduto grupoProduto) {
 		GrupoProdutoDao dao = new GrupoProdutoDao();
 		List<GrupoProduto> ret = new ArrayList<GrupoProduto>();
-		if ((grupoProduto.getId() == null)&&(grupoProduto.getNome() == null)&&(grupoProduto.getDescricao() == null)&&(grupoProduto.getGrupoPai() == null)){
+		grupoProduto.setId(grupoProduto.getId() != null && grupoProduto.getId().intValue() == 0 ? null : grupoProduto.getId());
+		if ((grupoProduto.getId() == null)				
+				&&((grupoProduto.getNome() == null)||("".equals(grupoProduto.getNome())))
+				&&((grupoProduto.getDescricao() == null)||("".equals(grupoProduto.getDescricao())))
+				&&((grupoProduto.getGrupoPai() == null)||(grupoProduto.getGrupoPai().intValue() == 0))){
 			ret = dao.getAll();
 		}else{
 			ret = dao.findByExample(grupoProduto);
