58cf82da8b9a44049a78b1abb252389b6583e6fe
jfeliperc <jfeliperc@gmail.com>
2015-06-23 08:07:31 -0300

    Home 23062015

---
+++ japp/JavaSource/com/module/jpa/dao/CardapioDao.java
@@ -0,0 +1,15 @@
+package com.module.jpa.dao;
+
+import java.util.List;
+
+import com.module.jpa.model.Acesso;
+import com.module.jpa.model.Pessoa;
+
+public class CardapioDao extends Dao<Acesso>{
+
+	public List<Acesso> getAcessoPorPessoa(Pessoa pessoa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
